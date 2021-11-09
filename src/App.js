35ed0c34ac832d186f1bc386ab6b7b5ca6608ec6
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-09 21:27:41 +0530

    displaying timer after finish using states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Form from './components/Form';
 import Timer from './components/Timer';
 import Finished from './components/Finished';
-import Block from './components/Block'
+import Block from './components/Block';
+
 function App() {
+  const [timerLeft, setTimerLeft] = useState(true);
   return (
     <>
-
+    <Form/>
     </>
   );
 }
