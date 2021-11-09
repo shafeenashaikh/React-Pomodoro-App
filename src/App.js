b38294f1cb4bec1b00dab30b94ec55492f854a3f
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-09 21:21:09 +0530

    imported all components in the app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import Form from './components/Form';
+import Timer from './components/Timer';
+import Finished from './components/Finished';
+import Block from './components/Block'
 function App() {
   return (
     <>
---
+++ src/components/Block.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Block() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Block
