cc909d2319b5644b6d02dd8602c5c513f9b412ac
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-10 12:07:37 +0530

    created header of the app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import Form from './components/Form';
 import Timer from './components/Timer';
 import Finished from './components/Finished';
 // import Block from './components/Block';
+import styled from 'styled-components';
+
+const Title = styled.div`
+    font-size: 50px;
+    text-align: center;
+    padding: 50px 0px;
+
+    background: pink;
+`
 
 function App() {
   const [timeLeft, setTimeLeft] = useState(0);
   const [isPaused, setIsPoused] = useState(true);
   return (
     <>
+    <Title>POMODORO APP</Title>
     <Form setTimeLeft={setTimeLeft}/>
     {
       timeLeft<0?
