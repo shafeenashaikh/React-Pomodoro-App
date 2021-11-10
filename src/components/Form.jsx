6ba9038e504bd739334562905fb10065dc649c7c
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-10 11:59:43 +0530

    created Form container

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {useForm} from 'react-hook-form';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display:flex;
+    justify-content: center;
+`
+
+const Text = styled.p`
+   margin: 0px 0px
+`
 
 function Form() {
     return (
-        <div>
-            FORM
-        </div>
+        <Container>
+            <Text>Minutes:</Text>
+            <form>
+                <input type="number"></input>
+                <input type="submit"></input>
+            </form>
+        </Container>
     )
 }
 
